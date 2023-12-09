fd813ec700982bb7b79a89296a8c94fd71c0c3e6
= <gabriel.cirilo.rosa@gmail.com>
2023-12-09 15:18:35 -0300

    start the project

---
+++ src/main/java/com/desafiofacil/dasafiofacil/DasafioFacilApplication.java
@@ -0,0 +1,13 @@
+package com.desafiofacil.dasafiofacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasafioFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasafioFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiofacil/dasafiofacil/DasafioFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiofacil.dasafiofacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasafioFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
